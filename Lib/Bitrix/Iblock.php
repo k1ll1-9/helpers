478cc8245911055d8ba3d4bc58a342ee80e4e972
k1ll1 <blazehardtoerase@gmail.com>
2019-02-26 09:39:46 +0300

    v0.0.3

--- Lib/Bitrix/Iblock.php
+++ Lib/Bitrix/Iblock.php
@@ -4,31 +4,19 @@ declare(strict_types=1);
 namespace K11\Bitrix;
 
 use Bitrix\Iblock\IblockTable;
+use Bitrix\Main\Loader;
 
 class Iblock
 {
 	/**
-	 * @return mixed|string
-	 */
-	public static function switchLanguage()
-	{
-		global $APPLICATION;
-
-		$url = $APPLICATION->GetCurPage(false);
-		if (LANGUAGE_ID === 'ru') {
-			return \str_replace('/ru/', '/', $url);
-		} else {
-			return '/ru' . $url;
-		}
-	}
-
-	/**
 	 * @param string $iblockCode
 	 * @return int|null
 	 * @throws \Bitrix\Main\ArgumentException
+	 * @throws \Bitrix\Main\LoaderException
 	 */
 	public static function iblockIdByCode(string $iblockCode)
 	{
+		Loader::includeModule('iblock');
 
 		$iblock = IblockTable::getList(
 			[
