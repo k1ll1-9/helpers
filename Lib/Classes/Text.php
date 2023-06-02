80160db16e7fdd8ec0d6f77fb4c9869d78dcda7d
k1ll1 <blazehardtoerase@gmail.com>
2023-06-02 16:34:38 +0300

    added numeral declination function

--- Lib/Classes/Text.php
+++ Lib/Classes/Text.php
@@ -9,7 +9,7 @@ Class Text{
      * @param $shortcutLength
      * @return array
      */
-    public static function getSplitShortcutText($text, $shortcutLength)
+    public static function getSplitShortcutText($text, $shortcutLength) : array
     {
         if (\strlen($text) <= $shortcutLength) {
 
@@ -30,5 +30,37 @@ Class Text{
             ];
 
         }
+    }
+    /**
+     * Склонение существительных после числительных.
+     *
+     * @param string $value Значение
+     * @param array $words Массив вариантов, например: array('товар', 'товара', 'товаров')
+     * @param bool $show Включает значение $value в результирующею строку
+     * @return string
+     */
+    public static function declineNumeral(string $value, array $words,bool $show = true) : string
+    {
+        $num = $value % 100;
+        if ($num > 19) {
+            $num = $num % 10;
+        }
+
+        $out = ($show) ? $value . ' ' : '';
+        switch ($num) {
+            case 1:
+                $out .= $words[0];
+                break;
+            case 2:
+            case 3:
+            case 4:
+                $out .= $words[1];
+                break;
+            default:
+                $out .= $words[2];
+                break;
+        }
+
+        return $out;
     }
 }
